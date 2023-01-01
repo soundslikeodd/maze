fadf3a41ad9150d6506ef0a4e004fe278ef61e5b
matthew schaadt <matthew@matthews-Air.home>
2022-12-31 20:37:49 -0800

    added reset button and updates style and controls

--- src/controls/Controls.jsx
+++ src/controls/Controls.jsx
@@ -6,19 +6,20 @@ const settingsString = (
         size,
         randomSE,
     }
-) => `${size.split(',').join(' by ')} Random Start/Finish: ${randomSE}`;
+) => `${size.split(',').join('x')} Random Start/Finish: ${randomSE}`;
 
 const SMALL = '10,10';
 const MEDIUM = '10,15';
 const LARGE = '15,15';
-const sizes = [
-    SMALL,
-    MEDIUM,
-    LARGE,
-];
+const sizes = {
+    small: SMALL,
+    medium: MEDIUM,
+    large: LARGE,
+};
 
 const Controls = ({
-    newMaze
+    newMaze,
+    restartMaze,
 }) => {
     const [randomSE, setRandomSE] = useState(false);
     const [size, setSize] = useState(SMALL);
@@ -34,59 +35,85 @@ const Controls = ({
         >
             <legend>Controls:</legend>
             <div
-                className="radio-container"
+                className="controls-row"
             >
-                <input 
-                    type="radio"
-                    checked={randomSE}
-                    onClick={() => setRandomSE(!randomSE)}
-                    onChange={() => setRandomSE(!randomSE)}
-                />
-                <label>
-                    Random Start/Finish
-                </label>
-            </div>
-            <div>
-                <label htmlFor="size-select">Size:</label>
-                <select 
-                    name="size"
-                    id="size-select"
-                    value={size}
-                    onChange={e => setSize(e.target.value)}
+                <div
+                    className="controls-col"
                 >
-                    {
-                        sizes.map(s => (
-                            <option
-                                key={s}
-                                value={s}
-                            >
-                                {s}
-                            </option>
-                        ))
-                    }
-                </select>
-            </div>
-            <button
-                onClick={() => {
-                    const [
-                        height,
-                        width,
-                    ] = size.split(',').map(s => Number(s));
-                    setCurrentSettings(
-                        settingsString({
-                            size,
-                            randomSE,
-                        })
-                    );
-                    newMaze(width, height, randomSE);
-                }}
-            >
-                New Maze
-            </button>
-            <div
-                className="current-settings"
-            >
-                {currentSettings}
+                    <div
+                        className="checkbox-container"
+                    >
+                        <input 
+                            type="checkbox"
+                            checked={randomSE}
+                            onClick={() => setRandomSE(!randomSE)}
+                            onChange={() => setRandomSE(!randomSE)}
+                        />
+                        <label>
+                            Random Start/Finish
+                        </label>
+                    </div>
+                    <div>
+                        <label htmlFor="size-select">Size:</label>
+                        <select 
+                            name="size"
+                            id="size-select"
+                            value={size}
+                            onChange={e => setSize(e.target.value)}
+                        >
+                            {
+                                Object.keys(sizes).map(s => (
+                                    <option
+                                        key={sizes[s]}
+                                        value={sizes[s]}
+                                    >
+                                        {s} - {sizes[s].split(',').join('x')}
+                                    </option>
+                                ))
+                            }
+                        </select>
+                    </div>
+                    <div
+                        className="current-settings"
+                    >
+                        {currentSettings}
+                    </div>
+                </div>
+                <div
+                    className="controls-col"
+                >   
+                    <div
+                        className="instructions"
+                    >
+                        navigate with arrow keys or swipe
+                    </div>
+                    <div
+                        className="controls-row buttons-container"
+                    >
+                        <button
+                            onClick={() => {
+                                const [
+                                    height,
+                                    width,
+                                ] = size.split(',').map(s => Number(s));
+                                setCurrentSettings(
+                                    settingsString({
+                                        size,
+                                        randomSE,
+                                    })
+                                );
+                                newMaze(width, height, randomSE);
+                            }}
+                        >
+                            New
+                        </button>
+                        <button
+                            onClick={restartMaze}
+                        >
+                            Restart
+                        </button>
+                    </div>
+                </div>
             </div>
         </fieldset>
     );
